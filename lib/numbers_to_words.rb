ded50029f8de73c492984d69918f476c9d2563da
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-23 15:00:18 -0800

    Convert num 8 to pass test
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -3,9 +3,7 @@ class NumConv
     @number = number.to_i
   end
   def number_convert
-    if (@number == 1)
-      result = "one"
-    end
-    result
+    digit_table = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    digit_table[@number]
   end
 end
