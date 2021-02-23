558b636146672351531ecc5bd029d5bb38eb9dc6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-23 14:45:55 -0800

    Convert num 1 to pass test
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,11 @@
 class NumConv
   def initialize(number)
-    @number = number
+    @number = number.to_i
   end
   def number_convert
+    if (@number == 1)
+      result = "one"
+    end
+    result
   end
 end
