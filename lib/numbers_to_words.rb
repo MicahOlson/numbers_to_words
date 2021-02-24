ce4a142c928231e9f2c9e1b4e5c1d4f81c0519bd
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-23 16:07:16 -0800

    Convert num 16 to pass test
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,9 +1,24 @@
 class NumConv
   def initialize(number)
-    @number = number.to_i
+    @number_array = number.split("").reverse()
   end
   def number_convert
-    digit_table = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-    digit_table[@number]
+    results = []
+    digit_table = {'1' => "one", '2' => "two", '3' => "three", '4' => "four", '5' => "five", '6' => "six", '7' => "seven", '8' => "eight", '9' => "nine"}
+    teens_table = {'1' => "eleven", '2' => "twelve",'3' => "thirteen",'4' => "fourteen",'5' => "fifteen",'6' => "sixteen",'7' => "seventeen", '8' => "eighteen",'9' => "nineteen"}
+    tens_table = {'2' => "twenty", '3' => "thirty", '4' => "forty", '5' => "fifty", '6' => "sixty", '7' => "seventy", '8' => "eighty", '9' => "ninety"}
+    if (@number_array.length >= 2)
+      if (@number_array[1] == '1')
+        results.push(teens_table[@number_array[0]])        
+      elsif
+        results.push(tens_table[@number_array[1]])
+      end
+    end
+    if (@number_array.length >= 1)
+      if (@number_array[1] != '1')
+        results.push(digit_table[@number_array[0]])
+      end
+    end
+    results.join()
   end
 end
