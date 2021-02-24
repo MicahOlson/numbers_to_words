a0993d451daad7ff9b1ef0578e911bf2a8d484bd
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-23 17:14:30 -0800

    Add zero to teens table
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -5,13 +5,15 @@ class NumConv
   def number_convert
     results = []
     digit_table = {'1' => "one", '2' => "two", '3' => "three", '4' => "four", '5' => "five", '6' => "six", '7' => "seven", '8' => "eight", '9' => "nine"}
-    teens_table = {'1' => "eleven", '2' => "twelve",'3' => "thirteen",'4' => "fourteen",'5' => "fifteen",'6' => "sixteen",'7' => "seventeen", '8' => "eighteen",'9' => "nineteen"}
+    teens_table = {'0' => 'ten','1' => "eleven", '2' => "twelve",'3' => "thirteen",'4' => "fourteen",'5' => "fifteen",'6' => "sixteen",'7' => "seventeen", '8' => "eighteen",'9' => "nineteen"}
     tens_table = {'2' => "twenty", '3' => "thirty", '4' => "forty", '5' => "fifty", '6' => "sixty", '7' => "seventy", '8' => "eighty", '9' => "ninety"}
-    
-    
-      
+
+
+
     if (@number_array.length >= 4)
+      if (@number_array[4] != '1')
       results.push(digit_table[@number_array[3] + 'thousand '])
+      end
     end
     if (@number_array.length >= 3)
       results.push(digit_table[@number_array[2] + 'hundred '])
